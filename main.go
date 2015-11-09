4951e15e955aa0fdd2de81f61dd31acafd54046c
Kayle Gishen <k@bkdsw.com>
2015-11-09 00:02:11 -0500

    Initial Commit

---
+++ main.go
@@ -0,0 +1,21 @@
+// Package main provides ...
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", hello)
+	fmt.Println("listening...")
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hello, world")
+}
